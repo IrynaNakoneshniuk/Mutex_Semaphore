92e8abbde562aa62c93e61f158dcde7dd67f6f18
IrynaNakoneshniuk <103773315+IrynaNakoneshniuk@users.noreply.github.com>
2023-01-19 00:00:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Mutex_Semaphore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[4];
+            RandomNumbers randomNumbers = new RandomNumbers();
+            PrimeNumbers primeNumbers = new PrimeNumbers();
+            ReportFiles reportFiles = new ReportFiles();
+            Mutex mutex = new Mutex();
+            try
+            {
+                threads[0] = new Thread(randomNumbers.WtiteRandomDigitInFile);
+                threads[1] = new Thread(primeNumbers.WritePrimeDigitInFile);
+                //threads[2] = new Thread(primeNumbers.WriteFileLastDigitsSeven);
+                //threads[3] = new Thread(reportFiles.Report);
+                for (int i = 0; i<2;i++)
+                {
+                    mutex.WaitOne();
+                    threads[i].Start();
+                    mutex.ReleaseMutex();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ RandomNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mutex_Semaphore
+{
+    public class RandomNumbers
+    {
+       public void WtiteRandomDigitInFile(object? data)
+       {
+            Mutex mutexObj = new Mutex();
+            List<int> list = new List<int>();
+            Random rd= new Random();
+            for(int i = 0; i < 100; i++)
+            {
+                list.Add(rd.Next(1,1000));
+            }
+            try
+            {
+                mutexObj.WaitOne();
+                using (FileStream fs = File.OpenWrite("RandomDigit.bin"))
+                {
+                    byte[] buffer = list.SelectMany(i => BitConverter.GetBytes(i)).ToArray();
+                    fs.Write(buffer, 0, buffer.Length);
+                }
+              
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);  
+            }
+            finally
+            {
+                mutexObj.ReleaseMutex();
+            }
+       }
+    }
+}
